bfc581909e4ffad393c2d7cc8d614c959511fce3
Raul Sanchez R. <raul.sr92@gmail.com>
2026-01-07 18:09:32 -0500

    Validacion de existencia de usuario a traves del correo en la BD

--- src/handlers/index.ts
+++ src/handlers/index.ts
@@ -14,14 +14,32 @@ export const createAccount = async(req: Request,res: Response)=>{
 
     console.log("Desde Register")
 
-    const {email} = req.body
+    // VERIFICACION DE USUARIO EXISTENTE
 
-    console.log(email)
+        // 1° Almacenar el email de todo el objeto enviado por el usuario
 
-    const userExists = await UserModel.findOne({email})
+                const {email} = req.body
+                console.log(email)
 
-    console.log(userExists)
+        // 2° Buscar el email en los registros de la base de datos
 
+                const userExists = await UserModel.findOne({email})
+                console.log(userExists)
+
+        // 3° Generar error en casi usuario exista
+
+                if (userExists) {
+
+                    const error = new Error("El usuario ya se encuentra registrado")
+
+                    console.log(error.name) // Error
+                    console.log(error.message)
+
+                    return res.status(409).json({error: error.message})
+
+                }        
+
+    // REGISTRAR USUARIO
 
     // Forma 1 para añadir usuarios
        //await UserModel.create(req.body)
@@ -30,10 +48,10 @@ export const createAccount = async(req: Request,res: Response)=>{
 
     // Forma 2 para añadir usuarios: instanciando el modelo
 
-        //const user = new UserModel(req.body)
+        const user = new UserModel(req.body)
 
-        //await user.save()
+        await user.save()
 
-    // res.send({mensaje:"Usuario creado con exito"})
+        res.send({mensaje:"Usuario creado con exito"})
 
 }
